afe8df92301f9f4fd30fdf60cc8f97fa8c57a3e8
JohnGuilding <jwguilding@gmail.com>
2024-07-06 13:01:34 +0200

    Add native safe recovery module

--- src/interfaces/ISafe.sol
+++ src/interfaces/ISafe.sol
@@ -5,4 +5,13 @@ interface ISafe {
     function swapOwner(address prevOwner, address oldOwner, address newOwner) external;
     function isOwner(address owner) external view returns (bool);
     function getOwners() external view returns (address[] memory);
+    function execTransactionFromModule(
+        address to,
+        uint256 value,
+        bytes memory data,
+        uint8 operation
+    )
+        external
+        returns (bool success);
+    function isModuleEnabled(address module) external view returns (bool);
 }
